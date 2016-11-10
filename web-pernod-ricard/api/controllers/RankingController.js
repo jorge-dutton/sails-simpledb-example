4edd82a30ad99a17051506f9ea8ac3fa8a1c5fe7
jorge-dutton <jorge_dutton@yahoo.com>
2016-11-10 08:42:02 +0100

    Check for user existence in POST request

--- web-pernod-ricard/api/controllers/RankingController.js
+++ web-pernod-ricard/api/controllers/RankingController.js
@@ -173,7 +173,7 @@ module.exports = {
 
         var findOne = function (err, data) {
             if (err) {
-                return res.status(409).json({ response: "KO", error: err });
+                res.status(409).json({ response: "KO", error: err });
             } else {
                 if (data.Items) {
                     resultItems = resultItems.concat(data.Items);
@@ -211,63 +211,86 @@ module.exports = {
     },
 
     createRankingItem: function (req, res) {
+
+        if (!req.param('email')) {
+            res.status(409).json({ response: 'KO', error: 'email param is mandatory for adding a new ranking user' });
+        }
+
         var AWS = require('aws-sdk'),
+            params = {},
             date = new Date(),
             lastUpdate =  date.getFullYear() + ':' +
                 ('0' + (date.getMonth() + 1)).slice(-2) + ':' +
                 ('0' + date.getDate()).slice(-2) + ':' +
                 ('0' + date.getHours()).slice(-2) + ':' +
                 ('0' + date.getMinutes()).slice(-2) + ':' +
-                ('0' + date.getSeconds()).slice(-2);
+                ('0' + date.getSeconds()).slice(-2);;
 
         AWS.config.update({accessKeyId: sails.config.aws.readAndWrite.accessKeyId, secretAccessKey: sails.config.aws.readAndWrite.secretKey});
         AWS.config.update({region: sails.config.aws.region, apiVersion: sails.config.aws.apiVersion});
 
         var simpledb = new AWS.SimpleDB();
 
-        var params = {
-            DomainName: 'rankingpernodricard',
-            ItemName: new Date().getTime().toString(),
-            Attributes: [
-                {
-                    Name: 'email',
-                    Value: req.param('email'),
-                    Replace: false
-                },
-                {
-                    Name: 'last_update',
-                    Value: lastUpdate,
-                    Replace: false
-                },
-                {
-                    Name: 'league',
-                    Value: req.param('league'),
-                    Replace: false
-                },
-                {
-                    Name: 'name',
-                    Value: req.param('name'),
-                    Replace: false
-                },
-                {
-                    Name: 'score',
-                    Value: req.param('score'),
-                    Replace: false
-                },
-                {
-                    Name: 'surname',
-                    Value: req.param('surname'),
-                    Replace: false
-                }
-            ]
+        params = {
+            SelectExpression: "select * from rankingpernodricard where `email` = '" + req.param("email") + "'",
+            ConsistentRead: true
         };
-        simpledb.putAttributes(params, function(err, data) {
-            if (err) {
-                res.status(409).json({ response: "KO" , error: err});
-            } else {
-                res.status(200).json({ response: "OK" , data: data});
-            }
-        });
+
+        /* Check if user already exists. If not we create the new user else we send back 409 error status */
+        var findOne = function (err, data) {
+
+            if (data.Items  ) {
+        	    res.status(409).json({ response: "KO", error: "User " + req.param("email") + "already exist" });
+		    } else {
+
+			    params = {
+				    DomainName: 'rankingpernodricard',
+				    ItemName: new Date().getTime().toString(),
+				    Attributes: [
+					    {
+						    Name: 'email',
+						    Value: req.param('email'),
+						    Replace: false
+					    },
+					    {
+						    Name: 'last_update',
+						    Value: lastUpdate,
+						    Replace: false
+					    },
+					    {
+						    Name: 'league',
+						    Value: req.param('league'),
+						    Replace: false
+					    },
+					    {
+						    Name: 'name',
+						    Value: req.param('name'),
+						    Replace: false
+					    },
+					    {
+						    Name: 'score',
+						    Value: req.param('score'),
+						    Replace: false
+					    },
+					    {
+						    Name: 'surname',
+						    Value: req.param('surname'),
+						    Replace: false
+					    }
+				    ]
+			    };
+
+			    simpledb.putAttributes(params, function (err, data) {
+				    if (err) {
+					    res.status(409).json({ response: "KO", error: err});
+				    } else {
+					    res.status(200).json({ response: "OK", data: data});
+				    }
+			    });
+		    }
+	    };
+
+	    simpledb.select(params, findOne);
 
     },
 
